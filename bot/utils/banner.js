1b47e9296364dcac62d38e327e1a6678f325eb0a
Anshuman Gahlaut <gahlautanshuman384@gmail.com>
2026-04-13 07:12:21 +0530

    style: remove Celestial Edition subtitle from banner

--- bot/utils/banner.js
+++ bot/utils/banner.js
@@ -1,6 +1,5 @@
 /**
  * JACK PREMIUM STARTUP BANNER
- * Generated specifically for a celestial/dark aesthetic.
  */
 
 const JACK_BANNER = `
@@ -11,13 +10,9 @@ const JACK_BANNER = `
 \\____/\\__,_/  \\___/ /_/|_|  \x1b[0m
 `;
 
-const SUBTITLE = "\x1b[90m « \x1b[35mCelestial Edition\x1b[0m \x1b[90m» \x1b[0m";
-
 module.exports = {
     getBanner: () => JACK_BANNER,
-    getSubtitle: () => SUBTITLE,
     print: () => {
-        console.log(JACK_BANNER);
-        console.log(" ".repeat(12) + SUBTITLE + "\n");
+        console.log(JACK_BANNER + "\n");
     }
 };
